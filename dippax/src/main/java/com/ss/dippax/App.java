a473e6da1f9e257e3e5b11037ab42f8caa114e47
Sujith Shivan <sujithshivanoffi@gmail.com>
2019-03-05 16:42:23 +0530

    Fist Commit Message

---
+++ dippax/src/main/java/com/ss/dippax/App.java
@@ -0,0 +1,13 @@
+package com.ss.dippax;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ dippaxfc/src/main/java/com/ss/dippaxfc/controller/PageController.java
@@ -0,0 +1,18 @@
+package com.ss.dippaxfc.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+public class PageController {
+	
+	@RequestMapping(value= {"/","/home","/index"})
+	public ModelAndView index()
+	{
+		ModelAndView mv=new ModelAndView("page");
+	    mv.addObject("Greetings","Succesful 1st step");
+	    return mv;
+	}
+
+}
